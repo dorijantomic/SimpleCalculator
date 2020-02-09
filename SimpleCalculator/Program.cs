801ae5c586d59389821a30d16c7ee472522fbe16
Dorijan Tomić <dorijantomic@outlook.com>
2020-02-09 10:40:44 +0100

    Created my first calculator console app

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = "Here is some text";
+            int number = 10;
+            double anotherNumber = 10.9;
+            bool isOredered = true;
+        }
+    }
+}
