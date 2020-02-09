e3756b9615866f0d7169ff273550ce6b52f82f9e
Dorijan Tomić <dorijantomic@outlook.com>
2020-02-09 10:41:19 +0100

    my first console app

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,10 +10,29 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            string someText = "Here is some text";
-            int number = 10;
-            double anotherNumber = 10.9;
-            bool isOredered = true;
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+                Console.WriteLine("Please enter the first number");
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please enter the second number");
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Please select the operation");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+           
+            } catch (Exception ex)
+            {
+                // In real world we would want to log this message
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+            
         }
     }
 }
