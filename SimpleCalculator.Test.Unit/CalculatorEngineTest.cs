7b56c2087b89ea11eb212b01c8c7a62952116112
Dorijan Tomić <dorijantomic@outlook.com>
2020-02-09 11:21:10 +0100

    created a unit test for calculator engine

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -4,11 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SimpleCalculator.Test.Unit
 {
     [TestClass]
-    public class UnitTest1
+    public class CalculatorEngineTest
     {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+
         [TestMethod]
-        public void TestMethod1()
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
         {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3,result);
+        }
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
         }
     }
 }
